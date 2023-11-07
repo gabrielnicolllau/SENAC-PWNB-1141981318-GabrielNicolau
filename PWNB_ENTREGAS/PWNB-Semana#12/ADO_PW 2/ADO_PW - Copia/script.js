5b01e31a4eee1591644e4c560a3736d3fd8f40ad
Gabriel Nicolau <117823819+gabrielnicolllau@users.noreply.github.com>
2023-11-06 23:56:42 -0300

    ADO feita (só arrumando os estilos)

--- PWNB_ENTREGAS/PWNB-Semana#12/ADO_PW 2/ADO_PW - Copia/script.js
+++ PWNB_ENTREGAS/PWNB-Semana#12/ADO_PW 2/ADO_PW - Copia/script.js
@@ -1,3 +1,4 @@
+// Declaração de variáveis
 const nome = document.querySelector("#nome");
 const sobrenome = document.querySelector("#sobrenome");
 const tipoCliente = document.querySelector("#tipo-cliente");
@@ -7,53 +8,67 @@ const cidade = document.querySelector("#cidade");
 const endereco = document.querySelector("#endereco");
 const numero = document.querySelector("#numero");
 
+// Regex para validar o nome
 const regNome = new RegExp("[A-z ]{6,100}");
 
+// Regex para validar o sobrenome
 const regSobrenome = new RegExp("[A-z ]{2,100}");
 
+// Regex para validar o CEP
 const regCep = new RegExp("[0-9]{5}-[0-9]{3}");
 
+// Regex para validar o tipo de cliente
 const regTipoCliente = new RegExp("^(ouro|prata|bronze)$");
 
-const regDataNascimento = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}");
+// Regex para validar a data de nascimento
+const regDataNascimento = /^(19\d{2}|20\d{2})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
 
+// Função para validar o formulário
 function validar() {
+    // Valida o nome
     if (!regNome.test(nome.value)) {
         alert("Informe um nome completo!");
         nome.focus();
         return false;
     }
 
+    // Valida o sobrenome
     if (!regSobrenome.test(sobrenome.value)) {
         alert("Informe um sobrenome!");
         sobrenome.focus();
         return false;
     }
 
+    // Valida o CEP
     if (!regCep.test(cep.value)) {
         alert("Informe um CEP válido!");
         cep.focus();
         return false;
     }
 
+    // Valida o tipo de cliente
     if (!regTipoCliente.test(tipoCliente.value)) {
         alert("Selecione um tipo de cliente válido!");
         tipoCliente.focus();
         return false;
     }
 
+    // Valida a data de nascimento
     if (!regDataNascimento.test(dataNascimento.value)) {
-        alert("Informe uma data de nascimento válida!");
+        alert("Informe uma data de nascimento válida: AAAA-MM-DD!");
         dataNascimento.focus();
         return false;
     }
 
+    // Se todas as validações forem aprovadas, retorna true
     return true;
 }
 
+// Array para armazenar os clientes cadastrados
 const clientes = [];
 let clienteIndex = -1;
 
+// Função para adicionar um cliente à lista
 function adicionarCliente(cliente) {
   if (clienteIndex === -1) {
     clientes.push(cliente);
@@ -66,6 +81,7 @@ function adicionarCliente(cliente) {
   atualizarListaClientes();
 }
 
+// Função para criar e adicionar um botão "Excluir" a um cliente na lista
 function adicionarBotaoExcluir(cliente, index) {
   const listaClientes = document.getElementById("clientes");
   const itemLista = document.createElement("li");
@@ -84,11 +100,13 @@ function adicionarBotaoExcluir(cliente, index) {
   listaClientes.appendChild(itemLista);
 }
 
+// Função para excluir um cliente da lista
 function excluirCliente(index) {
   clientes.splice(index, 1);
   atualizarListaClientes();
 }
 
+// Função para preencher o formulário com os dados de um cliente
 function preencherFormulario(index) {
   const cliente = clientes[index];
   document.getElementById("nome").value = cliente.nome;
@@ -104,6 +122,7 @@ function preencherFormulario(index) {
   document.getElementById("salvar").style.display = "none";
 }
 
+// Função para atualizar a lista de clientes na página
 function atualizarListaClientes() {
   const listaClientes = document.getElementById("clientes");
   listaClientes.innerHTML = "";
@@ -112,6 +131,7 @@ function atualizarListaClientes() {
   });
 }
 
+// Função para salvar um cliente
 function salvarCliente() {
   const nome = document.getElementById("nome").value;
   const sobrenome = document.getElementById("sobrenome").value;
@@ -139,6 +159,7 @@ function salvarCliente() {
   }
 }
 
+// Função para alterar um cliente
 function alterarCliente() {
   const nome = document.getElementById("nome").value;
   const sobrenome = document.getElementById("sobrenome").value;
